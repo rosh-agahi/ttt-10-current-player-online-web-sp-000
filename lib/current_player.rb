84b551a3ced67b3d3d82241de7c02cf197c1a265
Roshanak Agahi <rosh.agahi@gmail.com>
2019-08-21 22:22:48 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,16 +1,17 @@
 def turn_count(board)
   occupied = 0 
   turn_counter = board.each do |position| 
-    
      if position == "X" || position == "O"
       occupied += 1
     end
-      
   end
   occupied
 end
 
-def current_player
-
+def current_player(occupied)
+if occupied % 2 == 0 
+  return "O"
+else
+  return "X"
 end
 
